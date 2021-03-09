d4950368c26a25a5754501702f7cab6136560871
lpshkn <kofffsh@gmail.com>
2021-03-10 01:59:16 +0300

    Update the Bot module: add processing the messages and a loop of clients if the client will became is inactive

--- gecore/requests/bot_chat.py
+++ gecore/requests/bot_chat.py
@@ -1,3 +1,8 @@
+import asyncio
+import re
+from telethon import events
+
+
 class BotChat:
     def __init__(self, name: str, clients: list):
         """
@@ -5,10 +10,16 @@ class BotChat:
         :param name:
         :param clients:
         """
+        # Add event handler for each client
+        for client in clients:
+            client.add_event_handler(self._handle_message,
+                                     events.NewMessage(chats=[name], incoming=True, outgoing=False))
+
         self.name = name
         self.clients = clients
         self.is_active = True
         self.response = None
+        self.not_queries = False
 
     async def request(self, number):
         """
@@ -16,4 +27,27 @@ class BotChat:
         :param number:
         :return:
         """
-        await self.client.send_message(self.name, number)
+        for client in self.clients:
+            await client.send_message(self.name, number)
+
+            # response is None only if the message wasn't processed
+            # not_queries is True if the client has no queries more to the bot
+            while True:
+                if self.response is not None or self.not_queries:
+                    break
+                await asyncio.sleep(0.1)
+
+            if self.response is not None:
+                return self.response
+
+        return "У вас нет запросов к этому боту"
+
+    async def _handle_message(self, event):
+        if re.search(r'запросов не осталось', event.text, re.IGNORECASE):
+            self.not_queries = True
+        elif re.search(r'(некорректный номер|настоящий getcontact|ничего не найдено)',
+                     event.text, re.IGNORECASE):
+            self.response = []
+        elif event.is_reply and re.search('результаты', event.text, re.IGNORECASE):
+            text = re.sub(r"Результаты по \+?[\d]{11}:", '', event.text)
+            self.response = [name.strip() for name in text.split('\n') if name]
