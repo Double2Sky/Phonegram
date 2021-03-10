17378474b45aba18c3a72d832333c1e6772778b7
lpshkn <kofffsh@gmail.com>
2021-03-10 22:34:36 +0300

    Add timeout to the class
    Timeout is 1 minute. When this time will pass the method will return the message about it

--- gecore/requests/bot_chat.py
+++ gecore/requests/bot_chat.py
@@ -1,9 +1,12 @@
 import asyncio
 import re
+from datetime import datetime, timedelta
 from telethon import events
 
 
 class BotChat:
+    timeout = 60
+
     def __init__(self, name: str, clients: list):
         """
 
@@ -29,6 +32,7 @@ class BotChat:
         """
         for client in self.clients:
             await client.send_message(self.name, number)
+            time = datetime.now()
 
             # response is None only if the message wasn't processed
             # not_queries is True if the client has no queries more to the bot
@@ -36,6 +40,10 @@ class BotChat:
                 if self.response is not None or self.not_queries:
                     break
                 await asyncio.sleep(0.1)
+
+                # Check timeout time of answer
+                if datetime.now() > time + timedelta(seconds=self.timeout):
+                    return "Время ожидания ответа истекло"
 
             if self.response is not None:
                 return self.response
@@ -46,7 +54,7 @@ class BotChat:
         if re.search(r'запросов не осталось', event.text, re.IGNORECASE):
             self.not_queries = True
         elif re.search(r'(некорректный номер|настоящий getcontact|ничего не найдено)',
-                     event.text, re.IGNORECASE):
+                       event.text, re.IGNORECASE):
             self.response = []
         elif event.is_reply and re.search('результаты', event.text, re.IGNORECASE):
             text = re.sub(r"Результаты по \+?[\d]{11}:", '', event.text)
