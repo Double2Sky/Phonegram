50d420828008ee1d4eae3f4e6d558551c897b4e8
lpshkn <kofffsh@gmail.com>
2021-03-16 01:27:53 +0300

    Add the \n in the end of the response

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def main():
                                         bots=console_handler.bots)
         await requester.run()
         response = await requester.request(console_handler.phone_number)
-        console_handler.to_out(json.dumps(response, ensure_ascii=False, indent=4))
+        console_handler.to_out(json.dumps(response, ensure_ascii=False, indent=4) + '\n')
 
 
 if __name__ == '__main__':
