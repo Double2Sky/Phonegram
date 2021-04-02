f5478946e7b060679bc96b38ae562331c202b9a7
Lpshkn <lepkirill@yandex.ru>
2021-04-03 01:32:15 +0300

    Edit the module because of the main logic of work with the session config file was moved into the session.py module

--- phonegram/requests/requester.py
+++ phonegram/requests/requester.py
@@ -3,23 +3,21 @@ import sys
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from phonegram.console.console_handler import SESSION_STRINGS_SECTION, ConsoleHandler
 from phonegram.requests.bot_chat import BotChat
+from phonegram.config.session import SessionConfig
 
 
 logging.basicConfig(format='[%(asctime)s] MESSAGE:\n%(message)s\n',
                     level=logging.WARNING)
 
 
-class GetContactRequester:
-    client: TelegramClient
-
-    def __init__(self, console_handler: ConsoleHandler, bots: dict):
+class Requester:
+    def __init__(self, session_config: SessionConfig, bots: dict):
         """
         :param chats: a list of chats that will be listened by this client
         :param console_handler: ConsoleHandler object
         """
-        self._console_handler = console_handler
+        self._session_config = session_config
         self._bots = []
         self._clients = []
         self._bots_config = bots
@@ -31,17 +29,20 @@ class GetContactRequester:
         Also method initializes a dictionary containing the names of bots and list of TelegramClient objects.
         """
         clients = []
-        parser = self._console_handler.config_parser
-        for username, session_string in parser[SESSION_STRINGS_SECTION].items():
-            client = TelegramClient(StringSession(session_string), *self._console_handler.credentials)
+        for user_id, session_string in self._session_config.session_strings:
+            api_id = self._session_config.api_id
+            api_hash = self._session_config.api_hash
+            client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
             try:
                 # Make connection: if the session string is correct, connection will be made without logging
                 await client.start(phone=lambda: input("Пожалуйста, введите ваш номер телефона: "),
                                    code_callback=lambda: input("Пожалуйста, введите полученный код подтверждения: "))
             except Exception:
-                print(f'Для пользователя {username} session_string недействительна и будет удалена.', file=sys.stderr)
-                parser.remove_option(SESSION_STRINGS_SECTION, username)
+                print(f'Для пользователя с {user_id = } session_string недействительна и будет удалена.',
+                      file=sys.stderr)
+
+                self._session_config.remove_session_string(user_id)
                 continue
 
             # Make a list of clients
@@ -49,13 +50,12 @@ class GetContactRequester:
 
             # Update the parser object: maybe new updates were occurred
             session_string = client.session.save()
-            parser.set(SESSION_STRINGS_SECTION, username, session_string)
+            self._session_config.add_session_string(user_id, session_string)
 
         # Initialize the list of bots and make dump of the session config file
         for name in self._names_bots:
             bot_chat = BotChat(name, clients, self._bots_config[name])
             self._bots.append(bot_chat)
-        self._console_handler.dump_session_file()
         self._clients = clients
 
     async def request(self, phone_number: str):
