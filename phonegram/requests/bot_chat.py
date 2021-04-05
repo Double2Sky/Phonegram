e1ad436acc3ce7afb71a633d00e2f2795e1d4cc2
Lpshkn <lepkirill@yandex.ru>
2021-04-05 20:16:59 +0300

    Fix merging

--- phonegram/requests/bot_chat.py
+++ phonegram/requests/bot_chat.py
@@ -2,11 +2,7 @@ import asyncio
 import re
 from datetime import datetime, timedelta
 from telethon import events
-<<<<<<< HEAD:phonegram/requests/bot_chat.py
-from phonegram.utils import get_regex
-=======
 from phonegram.utils.utils import get_regex
->>>>>>> develop:gecore/requests/bot_chat.py
 from telethon.errors.rpcerrorlist import UsernameInvalidError
 
 # The defined parameters in the bots config file
